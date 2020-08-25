cbd68244757488f91cd1d80be716dc8cf15e351d
MuratTurkoz <44347576+MuratTurkoz@users.noreply.github.com>
2020-08-26 01:45:37 +0300

    Core

---
+++ MyNoteSample.Core/DataAccess/IDataAccess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+
+namespace MyNoteSample.Core.DataAccess
+{
+    public interface IDataAccess<T>
+    {
+        List<T> List();
+        IQueryable<T> ListQueryable();
+        List<T> List(Expression<Func<T, bool>> expression);
+        int Save();
+        int Update(T obj);
+        int Insert(T objec);
+        int Delete(T obj);
+        T Find(Expression<Func<T, bool>> expression);
+    }
+}
